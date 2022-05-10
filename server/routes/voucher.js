5615644c5619c8261733ec2f055ea5649d0cf783
Matei Adrian <57460362+matei4adrian@users.noreply.github.com>
2022-05-11 01:47:19 +0300

    Add middleware on vouchers routes

--- server/routes/voucher.js
+++ server/routes/voucher.js
@@ -1,16 +1,24 @@
 const express = require("express");
 const router = express.Router();
-
 const multerUpload = require("../controllers/other").uploadFile;
 const voucherController = require("../controllers").voucher;
+const otherController = require("../controllers").other;
 
-router.get("/", voucherController.getAll);
-router.post("/", multerUpload.single("fotografie"), voucherController.add);
+router.get("/", otherController.isLoggedIn, voucherController.getAll);
+router.post(
+  "/",
+  [otherController.isLoggedIn, multerUpload.single("fotografie")],
+  voucherController.add
+);
 router.put(
   "/:voucherId",
-  multerUpload.single("fotografie"),
+  [otherController.isLoggedIn, multerUpload.single("fotografie")],
   voucherController.update
 );
-router.delete("/:voucherId", voucherController.delete);
+router.delete(
+  "/:voucherId",
+  otherController.isLoggedIn,
+  voucherController.delete
+);
 
 module.exports = router;
